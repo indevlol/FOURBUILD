ae5a19353797697c97919d676c7b35c21e62417b
TheWhiteThaer <167879057+TheWhiteThaer@users.noreply.github.com>
2024-05-13 17:01:36 +0300

    Update PlayState.hx
    Made the score text lower 
    if it is wrong tell me I did it using my phone lol

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -562,7 +562,7 @@ class PlayState extends MusicBeatState
 		iconP2.visible = !ClientPrefs.data.hideHud;
 		iconP2.alpha = ClientPrefs.data.healthBarAlpha;
 
-		scoreTxt = new FlxText(0, healthBar.y + 140, FlxG.width, "", 20);
+		scoreTxt = new FlxText(0, healthBar.y + 240, FlxG.width, "", 20);
 		scoreTxt.setFormat(Paths.font("vcr.ttf"), 20, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		scoreTxt.scrollFactor.set();
 		scoreTxt.borderSize = 1.25;
